1fa4ca6e2eb32bdfd4c19ef8685c0c646508ecdc
TungNguynz <tungtdh2004@gmail.com>
2025-03-06 20:17:39 +0700

    xep loai hoc sinh dung

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -4,10 +4,10 @@ void main() {
 	float a;
 	printf("Hay nhap diem cua ban:");
 	scanf_s("%d", &a);
-	if (8.0 > a >= 6.5) {
+	if (8.0 > a  && a>= 6.5) {
 		printf("ban la hoc sinh kha");
 	}
-	else if (6.5>a>=5,0){
+	else if (6.5>a && a>=5,0){
 		printf("ban la hoc sinh trung binh");
 	}
 	else if (5.0 > a){
