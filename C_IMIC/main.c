08b03c9b1a8afb520549117e086907acb007fe1e
TungNguynz <tungtdh2004@gmail.com>
2025-03-06 19:50:56 +0700

    so sanh a va b

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-void main()
-{
-	printf("Xin chao  \n");
+void main() {
+	int a, b;
+	printf("Hay nhap gia tri cua a:");
+	scanf_s("%d", &a);
+	printf("Hay nhap gia tri cua b:");
+	scanf_s("%d", &b);
+	if (a < b) {
+		printf("a nho hon b");
+	}
+	else {
+		printf("a lon hon b");
+	}
 }
