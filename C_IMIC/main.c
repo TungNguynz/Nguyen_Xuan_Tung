837ae7eba81abb0b969f734d80a3945e94025112
TungNguynz <tungtdh2004@gmail.com>
2025-03-07 14:04:27 +0700

    find max a,b,c

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,32 +1,15 @@
 #include <stdio.h>
 
-void main() {
-	int buoc = 0, i =0, mat_khau = 1234, mat_khau_user, so_tien_rut=0;
-	switch (buoc) {
-	case 0:
-		while (i < 3) {
-			printf("Hay nhap mat khau cua ban:");
-			scanf_s("%d", &mat_khau_user);
-			if (mat_khau_user != mat_khau) {
-				i++;
-			}
-			else {
-				buoc++;
-				i = 4;
-			}
-		}
-	    case 1:
-			while (buoc == 1) {
-				printf("hay nhap so tien can rut: ");
-				scanf_s("%d", &so_tien_rut);
-				if (so_tien_rut < 10000) {
-					printf("so tien can rut la: %d VND", so_tien_rut);
-					buoc++;
-	
-				}
-			}
-			break;
+int main() {
+	int a, b, c;
+	printf("Hay nhap 3 so a,b,c:\n ");
+	scanf_s("%d %d %d", &a,&b,&c);
+	if (a >= b && a >= c) {
+		printf("%d la so lon nhat", a);
+	}else if (b >= c && b >= a) {
+		printf("%d la so lon nhat", b);
+	}
+	else if (c >=b && c >= a) {
+		printf("%d la so lon nhat", c);
 	}
 }
-
-
