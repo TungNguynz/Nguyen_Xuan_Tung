274a30bc573b4c6a44b635c243e8ce433797364f
TungNguynz <tungtdh2004@gmail.com>
2025-03-07 14:56:07 +0700

    kiem tra so am hay duong

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -2,20 +2,16 @@
 #include<math.h>
 
 int main() {
-	float a, b, c, delta, x1, x2, x;
-	printf("Hay nhap he so a,b,c:\n ");
-	scanf_s("%f %f %f", &a,&b,&c);
-	delta = (b * b) - (4 * a * c);
-
-	if (delta>0) {
-		x1 = (b * (-1) + sqrt(delta)) / (2 * a);
-		x2 = (b * (-1) - sqrt(delta)) / (2 * a);
-		printf("Phuong trinh co hai nghiem phan biet x1 va x2 lan luot la: %.2f va %.2f ", x1, x2);
-	}else if (delta==0) {
-		x = (b * (-1)) / (2 * a);
-		printf("phuonh trinh co ngiem duy nhat la x = %.2f", x);
+	int a;
+	printf("Hay nhap vao so nguyen a: ");
+	scanf_s("%d", &a);
+	if (a < 0) {
+		printf("%d la so am", a);
+	}
+	else if (a > 0) {
+		printf("%d la so duong", a);
 	}
 	else {
-		printf("phuong trinh vo nghiem");
+		printf("a la so 0");
 	}
 }
