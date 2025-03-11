1e2682187eb15bd40f30d4664ff5726eceacd044
TungNguynz <tungtdh2004@gmail.com>
2025-03-11 20:22:50 +0700

    nhap mk sai 3 lan

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,9 +1,15 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int main() {
-    int mk = 1234, mkuser=0;
+    int mk = 1234, mkuser=0, i=0;
     do {
         printf("hay nhap mat khau: ");
         scanf("%d", &mkuser);
+        if (mk != mkuser) {
+            i++;
+            if (i == 3) {
+                break;
+            }
+        }
     } while (mk != mkuser);
 }
