31971bc3e79a6236779fd8ca3021373861c1bc1b
TungNguynz <tungtdh2004@gmail.com>
2025-03-11 19:44:21 +0700

    tinh tien dien

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,15 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include<math.h>
-
 int main() {
-	int a;
-	printf("Hay nhap vao so nguyen a: ");
-	scanf_s("%d", &a);
-	if (a %2 == 0) {
-		printf("%d la so chan", a);
-	}
-	else if (a%2 != 0) {
-		printf("%d la so le", a);
-	}
-	
+    int kWh;
+    double tien_dien = 0; 
+    printf("Nhap so KWh su dung: ");
+    scanf("%d", &kWh);
+    if (kWh >= 0 && kWh <= 50) {
+        tien_dien = kWh * 1.678;
+    }
+    else if (kWh >= 51 && kWh <= 100) {
+        tien_dien = 50 * 1.678 + (kWh - 50) * 1.734;
+    }
+    else if (kWh >= 101 && kWh <= 200) {
+        tien_dien = 50 * 1.678 + 50 * 1.734 + (kWh - 100) * 2.014;
+    }
+    else if (kWh >= 201 && kWh <= 300) {
+        tien_dien = 50 * 1.678 + 50 * 1.734 + 100 * 2.014 + (kWh - 200) * 2.536;
+    }
+    else if (kWh >= 301 && kWh <= 400) {
+        tien_dien = 50 * 1.678 + 50 * 1.734 + 100 * 2.014 + 100 * 2.536 + (kWh - 300) * 2.834;
+    }
+    else if (kWh > 400) {
+        tien_dien = 50 * 1.678 + 50 * 1.734 + 100 * 2.014 + 100 * 2.536 + 100 * 2.834 + (kWh - 400) * 2.927;
+    }
+
+    printf("So tien phai tra cho %d KWh la: %.2f VND.\n", kWh, tien_dien);
+
+    return 0;
 }
