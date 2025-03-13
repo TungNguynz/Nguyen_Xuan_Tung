2102213743577555331baa763c3f4e022bf49ea4
TungNguynz <tungtdh2004@gmail.com>
2025-03-13 12:37:48 +0700

    tinh tong n so nguyen

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,35 +1,12 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int main() {
-    int sotien = 0, namchuc = 0, mottram = 0, haitram = 0, namtram = 0, tongATM = 0;
-    do {
-        printf("hay nhap so tien can rut: ");
-        scanf("%d", &sotien);
-    } while (sotien < 50000);
-        while (tongATM < sotien) {
-            if (sotien - tongATM >= 50000) {
-                namchuc++;
-                tongATM = namchuc * 50000 + mottram * 100000 + haitram * 200000 + namtram * 500000;
-
-            }
-            if (sotien - tongATM >= 100000) {
-                mottram++;
-                tongATM = namchuc * 50000 + mottram * 100000 + haitram * 200000 + namtram * 500000;
-
-            }
-            if (sotien - tongATM >= 200000) {
-                haitram++;
-                tongATM = namchuc * 50000 + mottram * 100000 + haitram * 200000 + namtram * 500000;
-
-            }
-            if (sotien - tongATM >= 500000) {
-                namtram++;
-                tongATM = namchuc * 50000 + mottram * 100000 + haitram * 200000 + namtram * 500000;
-
-            }
-        }
-        
-
-    printf(" co %d to 50, %d to 100, %d to 200, % d to 500", namchuc, mottram, haitram, namtram);
+    int n = 0, S=0;
+    printf("hay nhap so nguyen n: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        S = S + i;
+    }
+    printf(" tong cua %d so nguyen la: %d", n, S);
 }
-        
+    
