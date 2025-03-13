3bb1426730401e6e025074fccf6b5f7f4882c877
TungNguynz <tungtdh2004@gmail.com>
2025-03-13 13:27:40 +0700

    tinh so tien lai

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,13 +1,19 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int main() {
-    int n = 0, S=1;
-    printf("hay nhap so nguyen n: ");
-    scanf("%d", &n);
+    int n = 0, a=0;
+    float b = 0, S = 0;
+    printf("hay nhap so tien gui: ");
+    scanf("%d", &a);
+    printf("hay nhap so nam: ");
+    scanf("%d", &n); 
+    printf("hay nhap phan tram lai: ");
+    scanf("%f", &b);
+    S = a;
     for (int i = 1; i <= n; i++) {
         
-            S = S * i;
+            S = S + S*b;
     }
-    printf(" giao thua cua  %d la: %d", n, S);
+    printf( "tong so tien sau % d nam la : % f", n, S);
 }
     
