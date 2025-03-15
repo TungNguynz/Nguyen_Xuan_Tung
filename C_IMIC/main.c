a45869fb25f6f88974cfa40ef2114e0bf29a61eb
TungNguynz <tungtdh2004@gmail.com>
2025-03-15 20:27:17 +0700

    in ra cac gia tri le trong mang

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -4,13 +4,10 @@ int main() {
     int arr[] = { 1,-2,-2,4,4,-3,1,3,-2,-4,1,-3 };
     int so_luong_doi_tuong = sizeof(arr) / sizeof(arr[0]);
     for (int i = 0; i < so_luong_doi_tuong; i++) {
-        if (arr[i]<0) {
-             arr[i]=0;
+        if (arr[i]%2!=0) {
+             printf("%d ", arr[i]);
         }
     }
-    for (int i = 0; i < so_luong_doi_tuong; i++){
-            printf("%d ", arr[i]);
-         
-    }
+    
 }
     
