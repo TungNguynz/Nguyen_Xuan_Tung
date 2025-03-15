6ac63544d9812a2b3d6a83cbed384d920a66e0ff
TungNguynz <tungtdh2004@gmail.com>
2025-03-15 21:24:22 +0700

    in ra so lon nhat trong mang

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,23 +1,20 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include<math.h>
 int main() {
     int arr[] = { 1,2,2,4,4,3,1,3,2,4,1,3 };
+    int so_lon_nhat = 0;
     int so_luong_doi_tuong = sizeof(arr) / sizeof(arr[0]);
     for (int i = 0; i < so_luong_doi_tuong; i++) {
-        int a = 0;
-        if (arr[i]<=1) {}
-        else {
-            for (int n = 2; n <= sqrt(arr[i]); n++) {
-                if (arr[i]%n == 0) {
-                    a++;
-                }
-            }
-            if (a == 0) {
-                printf("%d", arr[i]);
-            }
+        if (so_lon_nhat < arr[i]) {
+            so_lon_nhat = arr[i];
         }
     }
-    
+    for (int i = 0; i < so_luong_doi_tuong; i++) {
+
+        if (arr[i] == so_lon_nhat) {
+
+            printf("%d ", arr[i]);
+            i = so_luong_doi_tuong;
+        }
+    }
 }
-    
