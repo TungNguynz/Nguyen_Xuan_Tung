68dca0a24c6b15358503a22810dddfb5e9485cee
TungNguynz <tungtdh2004@gmail.com>
2025-03-15 19:38:59 +0700

    tinh trung binh cac phan tu trong mang

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -3,6 +3,7 @@
 int main() {
     int array_1[10] = { 0 };
     int s = 0;
+    float n = 0;
     printf("hay nhap lan luot cac gia tri tu ban phim: ");
     for (int i = 0; i <= 9; i++) {
         scanf("%d", &array_1[i]);
@@ -10,6 +11,7 @@ int main() {
     for (int i = 0; i <= 9; i++) {
         s = s + array_1[i];
     }
-    printf("tong cac gia tri trong mang la %d", s);
+    n = (float)s / 10;
+    printf("gia tri trung binh cac phan tu trong mang la %.2f", n);
 }
     
