7e2d5b1fd7bfcdb35c6f0103fb29948063425850
TungNguynz <tungtdh2004@gmail.com>
2025-03-15 19:45:42 +0700

    tim trung binh so le trong mang

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -2,16 +2,19 @@
 #include <stdio.h>
 int main() {
     int array_1[10] = { 0 };
-    int s = 0;
+    int s = 0, a=0;
     float n = 0;
     printf("hay nhap lan luot cac gia tri tu ban phim: ");
     for (int i = 0; i <= 9; i++) {
         scanf("%d", &array_1[i]);
     }
     for (int i = 0; i <= 9; i++) {
-        s = s + array_1[i];
+        if (array_1[i] % 2 != 0) {
+            s = s + array_1[i];
+            a++;
+        }
     }
-    n = (float)s / 10;
-    printf("gia tri trung binh cac phan tu trong mang la %.2f", n);
+    n = (float)s / a;
+    printf("gia tri trung binh cac phan tu le trong mang la %.2f", n);
 }
     
