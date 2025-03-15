a09493104964c67221787d37aeff191910437bc7
TungNguynz <tungtdh2004@gmail.com>
2025-03-15 21:07:06 +0700

    kiem tra so nguyen to trong mang

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,11 +1,21 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include<math.h>
 int main() {
-    int arr[] = { 1,-2,-2,4,4,-3,1,3,-2,-4,1,-3 };
+    int arr[] = { 1,2,2,4,4,3,1,3,2,4,1,3 };
     int so_luong_doi_tuong = sizeof(arr) / sizeof(arr[0]);
     for (int i = 0; i < so_luong_doi_tuong; i++) {
-        if (arr[i]%2!=0) {
-             printf("%d ", arr[i]);
+        int a = 0;
+        if (arr[i]<=1) {}
+        else {
+            for (int n = 2; n <= sqrt(arr[i]); n++) {
+                if (arr[i]%n == 0) {
+                    a++;
+                }
+            }
+            if (a == 0) {
+                printf("%d", arr[i]);
+            }
         }
     }
     
