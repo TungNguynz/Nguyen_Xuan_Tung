ff9774bc9b0e82e79b8eb4a1a352122f15c555b9
TungNguynz <tungtdh2004@gmail.com>
2025-03-15 20:09:30 +0700

    thay so am thanh so 0 trong mang

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,19 +1,16 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int main() {
-    int arr[] = { 1,2,2,4,4,3,1,3,2,4,1,3 };
-    int so_lon_nhat = 0;
+    int arr[] = { 1,-2,-2,4,4,-3,1,3,-2,-4,1,-3 };
     int so_luong_doi_tuong = sizeof(arr) / sizeof(arr[0]);
     for (int i = 0; i < so_luong_doi_tuong; i++) {
-        if (so_lon_nhat < arr[i]) {
-            so_lon_nhat = arr[i];
+        if (arr[i]<0) {
+             arr[i]=0;
         }
     }
     for (int i = 0; i < so_luong_doi_tuong; i++){
-
-        if (arr[i] == so_lon_nhat) {
-            printf("%d ", i);
-        }
+            printf("%d ", arr[i]);
+         
     }
 }
     
