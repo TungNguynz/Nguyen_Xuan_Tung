d8325160e0fa1319d37d69de5b2c7b54983fb88d
TungNguynz <tungtdh2004@gmail.com>
2025-03-18 17:31:15 +0700

    hoan vi 2 so nguyen

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,20 +1,16 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
 int main() {
-    int arr[] = { 1,2,2,4,4,3,1,3,2,4,1,3 };
-    int so_lon_nhat = 0;
-    int so_luong_doi_tuong = sizeof(arr) / sizeof(arr[0]);
-    for (int i = 0; i < so_luong_doi_tuong; i++) {
-        if (so_lon_nhat < arr[i]) {
-            so_lon_nhat = arr[i];
-        }
-    }
-    for (int i = 0; i < so_luong_doi_tuong; i++) {
-
-        if (arr[i] == so_lon_nhat) {
-
-            printf("%d ", arr[i]);
-            i = so_luong_doi_tuong;
-        }
-    }
-}
+    int a, b, temp=0;
+    printf("Nhap so nguyen a: ");
+    scanf("%d", &a);
+    printf("Nhap số nguyen b: ");
+    scanf("%d", &b);
+    temp = a;
+    a = b;
+    b = temp;
+    printf("Sau khi hoan vi:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+}    
