f615a93ce5cdcc24f830bd6a491302a3b43720a7
TungNguynz <tungtdh2004@gmail.com>
2025-03-18 17:51:43 +0700

    sap xep cac phan tu mang

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -2,15 +2,25 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, temp=0;
-    printf("Nhap so nguyen a: ");
-    scanf("%d", &a);
-    printf("Nhap số nguyen b: ");
-    scanf("%d", &b);
-    temp = a;
-    a = b;
-    b = temp;
-    printf("Sau khi hoan vi:\n");
-    printf("a = %d\n", a);
-    printf("b = %d\n", b);
-}    
+    int arr[] = { 1,2,2,4,4,3,1,3,2,4,1,3 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i, j, min, temp;
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if (min != i) {
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    printf("Mang sau khi sap xep:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
