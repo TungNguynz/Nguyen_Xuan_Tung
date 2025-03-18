8c3d665eece368477b179340165d332fc9297883
TungNguynz <tungtdh2004@gmail.com>
2025-03-18 18:31:00 +0700

    in hoa chu dau

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -2,19 +2,15 @@
 #include <stdio.h>
 
 int main() {
-    char str[] = "Xin chao cac ban";  
-    int n = 0;  
+    char str[] = "nguyen van a"; 
     int i = 0;
-    int inword = 0;  
     while (str[i] != '\0') {
-        if (str[i] != ' ' && inword==0) {  
-            n++;  
-            inword = 1;  
-        }
-        else if (str[i] == ' ') {
-            inword = 0;  
+        if (i == 0 || str[i - 1] == ' ') {
+            if (str[i] >= 'a' && str[i] <= 'z') {
+                str[i] = str[i] - 32;  
+            }
         }
         i++;
     }
-    printf("So tu trong chuoi là: %d\n", n);
+    printf("Chuoi sau khi in hoa chu cai đau moi tu: %s\n", str);
 }
