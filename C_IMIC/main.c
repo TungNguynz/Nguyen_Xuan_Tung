d6cdbc289e64bd66b5c90bf239a358fe386e0cb2
TungNguynz <tungtdh2004@gmail.com>
2025-03-18 17:56:12 +0700

    dem so ki tu trong chuoi

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -2,25 +2,10 @@
 #include <stdio.h>
 
 int main() {
-    int arr[] = { 1,2,2,4,4,3,1,3,2,4,1,3 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int i, j, min, temp;
-    for (i = 0; i < n - 1; i++) {
-        min = i;
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min]) {
-                min = j;
-            }
-        }
-        if (min != i) {
-            temp = arr[i];
-            arr[i] = arr[min];
-            arr[min] = temp;
-        }
+    char str[] = "Hello";
+    int n = 0;
+    while (str[n] != '\0') {
+        n++;
     }
-    printf("Mang sau khi sap xep:\n");
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+    printf("So ky tu trong chuoi là: %d\n", n);
 }
