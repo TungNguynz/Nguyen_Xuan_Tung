02cdde0a54dfc37faf39e6edf4c7ae718badd2c4
TungNguynz <tungtdh2004@gmail.com>
2025-03-25 20:28:59 +0700

    hocsinh

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -1,13 +1,11 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
  
-struct phan_so {
-	int tu;
-	int mau;
-};
-struct phan_so nhan(struct phan_so a, struct phan_so b) {
-	struct phan_so ketqua;
-	ketqua.tu = a.tu * b.tu;
-	ketqua.mau = a.mau * b.mau;
-	return ketqua;
-}
+typedef struct {
+	char*ten;
+	int tuoi;
+	int toan;
+	int van;
+	float tb;
+	int xep_loai;
+}hoc_sinh;
