aabd8e8de892913163983c73c2437ca0a3e3d8e8
TungNguynz <tungtdh2004@gmail.com>
2025-03-25 20:59:30 +0700

    xeploai

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -4,8 +4,24 @@
 typedef struct {
 	char*ten;
 	int tuoi;
-	int toan;
-	int van;
+	float toan;
+	float van;
 	float tb;
 	int xep_loai;
 }hoc_sinh;
+float tb(float a, float b) {
+	int kq = (a + b) / 2;
+	return kq;
+}
+void main() {
+	hoc_sinh hoc_sinh[] = {
+		{.ten = "Nguyen Van A", .tuoi = 18, .toan = 7.5, .van = 6.0},
+		{.ten = "Nguyen Van B", .tuoi = 18, .toan = 7.5, .van = 6.0},
+		{.ten = "Nguyen Van C", .tuoi = 18, .toan = 7.5, .van = 6.0},
+		{.ten = "Nguyen Van D", .tuoi = 18, .toan = 7.5, .van = 6.0},
+	};
+	int n = sizeof(hoc_sinh) / sizeof(hoc_sinh[0]);
+	for (int i = 0; i < n; i++) {
+		hoc_sinh[i].tb = tb(hoc_sinh[i].toan, hoc_sinh[i].van)
+	}
+}
