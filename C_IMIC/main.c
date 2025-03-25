d91ce14fce3dfc217de7f83a2ef806c74a7f6317
TungNguynz <tungtdh2004@gmail.com>
2025-03-25 15:56:06 +0700

    timchuoiphu

--- C_IMIC/main.c
+++ C_IMIC/main.c
@@ -17,11 +17,30 @@ char* timc(char* str, char c) {
 		}
 	}
 	return 0;
-
+}
+char* Tim_Chuoi(char* str, char* subStr)
+{
+	int chieu_dai_str = sophantu(str);
+	int chieu_dai_subStr = sophantu(subStr);
+	for (int i = 0; i < chieu_dai_str; i++)
+	{
+		int j = 0;
+		for (; j < chieu_dai_subStr; j++)
+		{
+			if (str[i + j] != subStr[j])
+				break;
+		}
+		if (j == chieu_dai_subStr)
+			return str + i;
+	}
+	return 0;
 }
 int main() {
 	char* str = "hellcco";
 	char c = 'c';
+	char subStr[] = "he";
 	char* n = timc(str, c);
-	printf("%p", n);
+	char* vi_tri = Tim_Chuoi(str, subStr);
+	printf("%p\n", n);
+	printf("%p", vi_tri);
 }
